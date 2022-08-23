bd68d81fc1df4bd236d9d8ef943c8db3098e56fd
Shinshi <jiankey.zjgsu@gmail.com>
2022-08-23 17:38:15 +0800

    Update find_coi.py

--- find_coi.py
+++ find_coi.py
@@ -7,7 +7,7 @@ from xml.dom.minidom import parse
 parser = argparse.ArgumentParser()
 parser.add_argument('--xml_path', type=str, default='./data/',
                     help='the path of files to find coi. Currently only support the xml file from dblp')
-parser.add_argument('--search_years', type=int, default=10,
+parser.add_argument('--search_years', type=int, default=3,
                     help='number of years queried to check for COI.')
 parser.add_argument('--pc_file', type=str, default='./data/pc_members.xlsx',
                     help='file with all pc members')
@@ -16,7 +16,7 @@ def get_paper(path, query_years):
     xml_data = parse(path)
     rootNode = xml_data.documentElement
     nodes = list(rootNode.getElementsByTagName('article')) + list(rootNode.getElementsByTagName('inproceedings'))
-    coi_authors = set()
+    coi_authors = dict()
     
     for node in nodes:
         year = node.getElementsByTagName('year')[0].childNodes[0].data
@@ -25,8 +25,11 @@ def get_paper(path, query_years):
         authors = node.getElementsByTagName('author')
         for author in authors:
             name = author.childNodes[0].data.rstrip('0123456789').rstrip()
-            coi_authors.update([name])
-    return list(coi_authors)
+            if(name not in coi_authors):
+                coi_authors[name] = 1
+            else:
+                coi_authors[name] += 1
+    return coi_authors
 
 args = parser.parse_args()
 years = list(map(str,[i for i in range(time.localtime(time.time())[0] - args.search_years, time.localtime(time.time())[0]+1)]))
@@ -48,7 +51,7 @@ for row in pc_sheet.values:
         if(check_name in coi_authors_dict[name]):
             if(name not in result):
                 result[name] = list()
-            result[name].append(check_name)
+            result[name].append([check_name, coi_authors_dict[name][check_name]])
 for name in result:
     for coi in result[name]:
-        print(name + ' : ' + coi)
+        print(name + ' : ' + str(coi))
