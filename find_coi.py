cd9aab357f3e7082b8a3e999cbc8289c89e3781d
Shinshi <jiankey.zjgsu@gmail.com>
2022-08-31 10:27:18 +0800

    Update find_coi.py

--- find_coi.py
+++ find_coi.py
@@ -9,7 +9,7 @@ parser.add_argument('--xml_path', type=str, default='./data/',
                     help='the path of files to find coi. Currently only support the xml file from dblp')
 parser.add_argument('--search_years', type=int, default=3,
                     help='number of years queried to check for COI.')
-parser.add_argument('--years_and_times', type=list, default=[],
+parser.add_argument('-n', '--years_and_times', nargs='+', default=[],
                     help='[a, b] means co-auther >= a times in last b years.')
 parser.add_argument('--pc_file', type=str, default='./data/pc_members.xlsx',
                     help='file with all pc members')
@@ -66,12 +66,14 @@ def check(search_years, time_flag = None):
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    print(args)
     names = os.listdir(args.xml_path)
     result_dict1 = check(args.search_years)
     print('co-auther in last', args.search_years, 'year(s):')
     for name in result_dict1:
         print(name, ':', str(result_dict1[name]))
     if(args.years_and_times != []):
+        args.years_and_times = list(map(int, args.years_and_times))
         print()
         result_dict2 = check(args.years_and_times[1], time_flag = args.years_and_times[0])
         print('co-auther >=', args.years_and_times[0], 'time(s) in last', args.years_and_times[1], 'year(s):')
