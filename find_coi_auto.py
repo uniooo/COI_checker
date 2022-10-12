21bc67b6da7b9e6044b89e7d4f579cfa2f0388c7
yujianke100 <734962820@qq.com>
2022-10-12 14:57:43 +0800

    update

--- find_coi_auto.py
+++ find_coi_auto.py
@@ -6,14 +6,14 @@ import sys
 from xml.dom.minidom import parse
 import requests
 import shutil
-from dblp_xml import dblp_xml_dict
+from settings import *
 
 root_path, _ = os.path.split(os.path.abspath(__file__))
 parser = argparse.ArgumentParser()
-parser.add_argument('--search_years', type=int, default=2,
-                    help='number of years queried to check for COI.')
-parser.add_argument('-n', '--times_and_years', nargs='+', default=[],
-                    help='[a, b] means co-auther >= a times in last b years.')
+# parser.add_argument('--search_years', type=int, default=2,
+#                     help='number of years queried to check for COI.')
+# parser.add_argument('-n', '--times_and_years', nargs='+', default=[],
+#                     help='[a, b] means co-auther >= a times in last b years.')
 parser.add_argument('--pc_file', type=str, default=root_path+'/data/pc_members.xlsx',
                     help='file with all pc members')
 
@@ -90,6 +90,8 @@ def check(search_years, time_flag = None):
 if __name__ == '__main__':
     args = parser.parse_args()
     args.xml_path = root_path+'/tmp/'
+    args.search_years = search_years
+    args.times_and_years = times_and_years
     print(args)
     save_xml(dblp_xml_dict)
     names = os.listdir(args.xml_path)
@@ -103,7 +105,7 @@ if __name__ == '__main__':
     for name in result_dict1:
         print(name, ':', str(result_dict1[name]))
     if(args.times_and_years != []):
-        args.times_and_years = list(map(int, args.times_and_years))
+        args.times_and_years = times_and_years
         print()
         result_dict2 = check(args.times_and_years[1], time_flag = args.times_and_years[0])
         print('co-auther >=', args.times_and_years[0], 'time(s) in last', args.times_and_years[1], 'year(s):')
--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
+search_years = 2
+times_and_years = [4,10] #the number of co-auther times and years. e.g., 4 times in 10 years: [4, 10]
 dblp_xml_dict = {'ying_zhang':'https://dblp.org/pid/13/6769-1.xml',
                      'lu_qin':'https://dblp.org/pid/51/5585.xml'}
