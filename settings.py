de94ee443e8a5793577ade0a8af63d0b68f05c1b
yujianke100 <734962820@qq.com>
2022-10-12 15:36:08 +0800

    update

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 search_years = 2
 times_and_years = [4,10] #the number of co-auther times and years. e.g., 4 times in 10 years: [4, 10]
-dblp_xml_dict = {'ying_zhang':'https://dblp.org/pid/13/6769-1.xml',
-                     'lu_qin':'https://dblp.org/pid/51/5585.xml'}
+dblp_xml_dict = {
+    'ying_zhang':'https://dblp.org/pid/13/6769-1.xml',
+    'lu_qin':'https://dblp.org/pid/51/5585.xml',
+    'wenjie_zhang': 'https://dblp.org/pid/98/5684-1.xml',
+    'xiaoyang_wang':'https://dblp.org/pid/81/1832-2.xml',
+    } #find authors in dblp.org, and get their XML export bibliography url
