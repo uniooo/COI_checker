30d4c6bd4e5f3cf42c4fdd030836ec6a80b9a80c
Yu Jianke <jiankey.zjgsu@gmail.com>
2022-10-19 20:52:18 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 search_years = 2
-times_and_years = [4,10] #the number of co-auther times and years. e.g., 4 times in 10 years: [4, 10]
+times_and_years = [] #the number of co-auther times and years. e.g., 4 times in 10 years: [4, 10]
 dblp_xml_dict = {
     'ying_zhang':'https://dblp.org/pid/13/6769-1.xml',
     'lu_qin':'https://dblp.org/pid/51/5585.xml',
