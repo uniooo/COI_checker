22d8b250fbf944ccc99e36c7c34420b958e2275e
Yu Jianke <jiankey.zjgsu@gmail.com>
2022-11-29 17:52:49 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,9 +2,14 @@ search_years = 2
 times_and_years = [] #the number of co-auther times and years. e.g., 4 times in 10 years: [4, 10]
 dblp_xml_dict = {
     'ying_zhang':'https://dblp.org/pid/13/6769-1.xml',
-    'lu_qin':'https://dblp.org/pid/51/5585.xml',
+#     'lu_qin':'https://dblp.org/pid/51/5585.xml',
     'wenjie_zhang': 'https://dblp.org/pid/98/5684-1.xml',
-    'xiaoyang_wang':'https://dblp.org/pid/81/1832-2.xml',
-    'zhao_li':'https://dblp.org/pid/l/ZhaoLi-7.xml',
-    'jian_liao':'https://dblp.org/pid/72/186.xml'
+    'peilun_yang': 'https://dblp.uni-trier.de/pid/292/2928.xml',
+    'xuemin_lin1': 'https://dblp.uni-trier.de/pid/l/LinXuemin.xml',
+    'xuemin_lin2': 'https://dblp.uni-trier.de/pid/305/2626.xml',
+    'xuemin_lin3': 'https://dblp.uni-trier.de/pid/305/2626-3.xml',
+    'zhengping_qian' : 'https://dblp.uni-trier.de/pid/36/5395.xml',
+#     'xiaoyang_wang':'https://dblp.org/pid/81/1832-2.xml',
+#     'zhao_li':'https://dblp.org/pid/l/ZhaoLi-7.xml',
+#     'jian_liao':'https://dblp.org/pid/72/186.xml'
     } #find authors in dblp.org, and get their XML export bibliography url
